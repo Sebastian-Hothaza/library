0c28a6e21a2478f517058af8f2cba68c49e69b75
Sebastian <sebastianhothaza@gmail.com>
2022-11-09 20:02:21 -0500

    Final commit, all working

--- script.js
+++ script.js
@@ -13,87 +13,7 @@ const inputRead = document.querySelector('input[name="inputRead"]');
 
 const grid = document.querySelector('.bookGrid');
 
-
 const addBookBtn = document.querySelector('#addBookBtn');
-addBookBtn.addEventListener('click', e => {
-    
-
-    const newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
-    addBookToLibrary(newBook, myLibrary);
-
-    
-    // Reset fields
-    inputTitle.value="";
-    inputAuthor.value="";
-    inputPages.value="";
-    inputRead.checked=false;
-
-    // Append book to grid
-
-    // First we create the elements of the card
-    const newCardTitle = document.createElement('div');
-    newCardTitle.classList.add('title')
-    newCardTitle.textContent = newBook.title;
-
-    // Delete button
-    const newCardDelete = document.createElement('button');
-    newCardDelete.textContent = "X";
-    newCardTitle.appendChild(newCardDelete);
-    newCardDelete.addEventListener('click', e => {
-        console.log("KILL");
-    });
-
-
-
-    const newCardAuthor = document.createElement('div');
-    newCardAuthor.classList.add('author')
-    newCardAuthor.textContent = "Author: " + newBook.author;
-
-    const newCardPages = document.createElement('div');
-    newCardPages.classList.add('numPages')
-    newCardPages.textContent = "Pages: " + newBook.pages;
-
-    const newCardRead = document.createElement('div');
-    if (newBook.read){
-        newCardRead.classList.add('read');
-        newCardRead.textContent = "Read";
-    }else{
-        newCardRead.classList.add('unread');
-        newCardRead.textContent = "Not yet read!";
-    }
-
-    // Now, create the newCard
-    const newCard = document.createElement('div');
-    newCard.classList.add('card');
-
-    // Attach the created elements to the newCard
-    newCard.appendChild(newCardTitle);
-    newCard.appendChild(newCardAuthor);
-    newCard.appendChild(newCardPages);
-    newCard.appendChild(newCardRead);
-
-
-    // Attach the newCard to the grid
-    grid.appendChild(newCard);
-
-
-    // Update summary counter
-    document.querySelector('#totalBooks>div:nth-child(-n+1)').textContent = 1 + 
-        parseInt(document.querySelector('#totalBooks>div:nth-child(-n+1)').textContent);
-
-    document.querySelector('#totalPages>div:nth-child(-n+1)').textContent = parseInt(newBook.pages) + 
-        parseInt(document.querySelector('#totalPages>div:nth-child(-n+1)').textContent);
-
-    if (newBook.read){
-        document.querySelector('#booksRead>div:nth-child(-n+1)').textContent = 1 + 
-            parseInt(document.querySelector('#booksRead>div:nth-child(-n+1)').textContent);
-    }
-});
-
-const removeBookBtn = document.querySelector('#addBookBtn');
-removeBookBtn.addEventListener('click', e => {
-
-});
 
 // Book CTOR
 function Book(title, author, pages, read){
@@ -111,6 +31,17 @@ Book.prototype.toggleRead = function() {
     }
 }
 
+
+addBookBtn.addEventListener('click', e => {
+    // Create the book and append to the array
+    const newBook = new Book(inputTitle.value, inputAuthor.value, inputPages.value, inputRead.checked);
+    addBookToLibrary(newBook, myLibrary);
+    resetFields();
+    displayBooks();
+    updateSummary();
+});
+
+
 // Adds book to the myLibrary array
 function addBookToLibrary(Book, arr){
     arr.push(Book);
@@ -118,7 +49,107 @@ function addBookToLibrary(Book, arr){
 
 
 function displayBooks(){
-    for (let i=0; i<myLibrary.length; i++){
-        console.log(myLibrary[i]);
-    }
+    grid.textContent = "";
+    myLibrary.forEach((book, index) => {
+        // Append book to grid
+
+        // First we create the elements of the card
+        const newCardTitle = document.createElement('div');
+        newCardTitle.classList.add('title')
+        newCardTitle.textContent = book.title;
+
+        // Delete button
+        const newCardDelete = document.createElement('button');
+        const newCardDeleteIcon = document.createElement('span');
+
+        newCardDeleteIcon.classList.add('material-icons');
+        newCardDeleteIcon.textContent = "delete";
+        newCardDelete.appendChild(newCardDeleteIcon);
+        newCardTitle.appendChild(newCardDelete);
+        newCardDelete.addEventListener('click', e => {
+            removeBook(index);
+        });
+
+        const newCardAuthor = document.createElement('div');
+        newCardAuthor.classList.add('author')
+        newCardAuthor.textContent = "Author: " + book.author;
+
+        const newCardPages = document.createElement('div');
+        newCardPages.classList.add('numPages')
+        newCardPages.textContent = "Pages: " + book.pages;
+
+        const newCardRead = document.createElement('div');
+        if (book.read){
+            newCardRead.classList.add('read');
+            newCardRead.textContent = "Read";
+        }else{
+            newCardRead.classList.add('unread');
+            newCardRead.textContent = "Not yet read!";
+        }
+
+        // Mark read button
+        const newCardToggleRead = document.createElement('button');
+        const newCardToggleReadIcon = document.createElement('span');
+
+        newCardToggleReadIcon.classList.add('material-icons');
+        newCardToggleReadIcon.textContent = "menu_book";
+        newCardToggleRead.appendChild(newCardToggleReadIcon);
+
+        newCardRead.appendChild(newCardToggleRead);
+        newCardToggleRead.addEventListener('click', e => {
+            flipRead(index);
+        });
+
+        // Now, create the newCard
+        const newCard = document.createElement('div');
+        newCard.setAttribute('id',index); // Do we even really need this?
+        newCard.classList.add('card');
+
+        // Attach the created elements to the newCard
+        newCard.appendChild(newCardTitle);
+        newCard.appendChild(newCardAuthor);
+        newCard.appendChild(newCardPages);
+        newCard.appendChild(newCardRead);
+
+
+        // Attach the newCard to the grid
+        grid.appendChild(newCard);
+    });
+
+
+
+}
+
+function updateSummary(){
+    let numPages = 0;
+    let numRead = 0;
+    myLibrary.forEach((book) => {
+        numPages+=parseInt(book.pages);
+        if (book.read){
+            numRead++;
+        }
+    });
+    
+    document.querySelector('#totalBooks>div:nth-child(-n+1)').textContent = myLibrary.length;
+    document.querySelector('#totalPages>div:nth-child(-n+1)').textContent = numPages;
+    document.querySelector('#booksRead>div:nth-child(-n+1)').textContent = numRead;
+}
+
+function resetFields(){
+    inputTitle.value="";
+    inputAuthor.value="";
+    inputPages.value="";
+    inputRead.checked=false;
+}
+
+function removeBook(index){
+    myLibrary.splice(index, 1);
+    displayBooks();
+    updateSummary();
+}
+
+function flipRead(index){
+    myLibrary[index].toggleRead();
+    displayBooks();
+    updateSummary();
 }
